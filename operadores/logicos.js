7ef489e46e4d604f4ff14f4bc9ed8a60be52d4e0
AlicePlentz <alicecplentz@gmail.com>
2024-04-03 11:18:59 -0300

    operadores logicos 2

--- operadores/logicos.js
+++ operadores/logicos.js
@@ -1,6 +1,15 @@
+// && (and)
+
 let idade = 18 
 let vistoVerificado = true
 console.log ( (idade >= 18) && (vistoVerificado == true))
 
 console.log ("--------------------")
 
+// || (or)
+
+let tempo = "chuva"
+let item = "guarda chuva"
+let podeSair = (tempo != "chuva") || (item === "guarda chuva")
+
+console.log ("Nosso personagem pode sair? " + podeSair)
