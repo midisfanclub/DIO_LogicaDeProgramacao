0832d0264de3c6444a29df5afb51dcbb401d8055
AlicePlentz <alicecplentz@gmail.com>
2024-04-05 14:10:49 -0300

    Else if

---
+++ estruturas de decisão/else if.js
@@ -0,0 +1,16 @@
+let nivelFome = 5
+
+if (nivelFome === 1)
+{
+    console.log ("pouca fome")
+}
+
+else if (nivelFome === 2)
+{
+    console.log ("fome")
+}
+
+else
+{
+    console.log ("morreu de fome")
+}
