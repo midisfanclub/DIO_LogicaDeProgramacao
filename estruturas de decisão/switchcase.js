a3fa557a7f9ec763afe731c9d24f07e0f0913446
AlicePlentz <alicecplentz@gmail.com>
2024-04-05 14:27:31 -0300

    Switch case 1

---
+++ estruturas de decisão/switchcase.js
@@ -0,0 +1,21 @@
+// estrutura de decisão
+
+let fruta = "limão"
+
+switch (fruta)
+{
+    case "laranja":
+        console.log ("suco de laranja")
+        break
+
+    case "banana":
+        console.log ("vitamina de banana")
+        break
+
+    case "maçã":
+        console.log ("suco de maçã")
+        break
+
+    default:
+    console.log ("Suco de caixinha")
+}
