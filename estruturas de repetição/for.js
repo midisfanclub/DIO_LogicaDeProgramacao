0ea47b9a022d792218b77c8d16a7d2a6365eb8b3
AlicePlentz <alicecplentz@gmail.com>
2024-04-19 11:13:34 -0300

    Estruturas de repetição - For

---
+++ estruturas de repetição/for.js
@@ -0,0 +1,16 @@
+for (let contador = 0; contador <= 4; contador ++)
+{
+    console.log(contador)
+    
+    if (contador != 4)
+    {
+        console.log("Aumentar contagem")
+    }
+    
+    else 
+    {
+        console.log("Fim da contagem")
+    }
+}
+
+
