50a49638302be36b588bf79355e72018694e9bde
AlicePlentz <alicecplentz@gmail.com>
2024-05-02 15:54:29 -0300

    Desafio 1 - versão 1

---
+++ desafio1/script.js
@@ -0,0 +1,15 @@
+let nomeHeroi = "xulé"
+let xp = "ferro"
+
+switch (xp)
+{
+    case xp <= 1000:
+        console.log ("O Herói de nome " + {nomeHeroi} + "está no nível de Ferro")
+        break
+
+    case 1001 <= xp <= 2000:
+
+        
+        
+        
+}
