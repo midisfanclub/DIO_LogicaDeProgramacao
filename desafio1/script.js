f2074bad9447058abb4d7397451c32674b9fa30b
AlicePlentz <alicecplentz@gmail.com>
2024-05-02 15:55:07 -0300

    Desafio 1 - versão final

--- desafio1/script.js
+++ desafio1/script.js
@@ -1,15 +1,43 @@
-let nomeHeroi = "xulé"
-let xp = "ferro"
+let nomeHeroi = "Xulé"
+let xp = 7567
 
-switch (xp)
+switch (true)
 {
     case xp <= 1000:
-        console.log ("O Herói de nome " + {nomeHeroi} + "está no nível de Ferro")
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Ferro")
         break
 
-    case 1001 <= xp <= 2000:
+    case xp >= 1001 && xp <= 2000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Bronze")
+        break
 
-        
-        
+    case xp >= 2001 && xp <= 5000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Prata")
+        break
+
+    case xp >= 5001 && xp <= 7000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Ouro")
+        break
+
+    case xp >= 7001 && xp <= 8000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Platina")
+        break
+
+    case xp >= 8001 && xp <= 9000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Ascendente")
+        break
+
+    case xp >= 9001 && xp <= 10000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Imortal")
+        break
+
+    case xp > 10000:
+        console.log ("O Herói de nome " + nomeHeroi + " está no nível Radiante")
+        break
+
+    default:
+        console.log ("Nivel do herói desconhecido!")
         
 }
+        
+        
