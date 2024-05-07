adb10e71b676bd9847a6d32af733cd5dcc10798c
AlicePlentz <alicecplentz@gmail.com>
2024-05-07 17:03:14 -0300

    Funções aula 2

--- funções/funcoes1.js
+++ funções/funcoes1.js
@@ -1,3 +1,6 @@
+console.log("------------- Aula 1 funçoes -------------")
+console.log()
+
 function torrar ()
 {
     console.log("torrando pão")
@@ -13,3 +16,18 @@ function injetarPão()
 }
 
 injetarPão()
+
+console.log()
+console.log("------------- Aula 2 funçoes -------------")
+console.log()
+
+tostarPao("pão de forma integral")
+tostarPao("pão de forma australiano", "Thaís") //preenchendo o valor da viável tipoPao
+//uma função pode ter vários parâmetros
+
+function tostarPao(tipoPao, nome = "Cliente X") //variável tipoPao recebe o valor preenchido em cima (quando a função é chamada)
+// Cliente = valor default, se nenhum nome for passado, esse valor que vai aparecer na tela
+{
+    console.log("torrada feita com " + tipoPao)
+    console.log("A torrada vai ser comida por " + nome)
+}
