d462ded7a6c600954b7f4f47788273919025e817
AlicePlentz <alicecplentz@gmail.com>
2024-05-08 15:29:58 -0300

    Funções com retorno

---
+++ funções/funcaoComRetorno.js
@@ -0,0 +1,11 @@
+let resultado = soma(5, 10) //atribuindo a uma variável o resultado de uma função
+
+console.log ("resultado da função = " + resultado)
+// console.log("resultado da função = " + soma(5, 10)) --> menos legivwl
+
+function soma (numA, numB)
+{
+    return numA + numB
+    // let somatorio = numA + numB 
+    // return somatorio
+}
