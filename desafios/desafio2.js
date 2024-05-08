45e31b0fd65cc8d7783cfcbc1da209f5d571f41b
AlicePlentz <alicecplentz@gmail.com>
2024-05-08 16:29:12 -0300

    Desafio 2 - finalizado

---
+++ desafios/desafio2.js
@@ -0,0 +1,49 @@
+let saldo = saldoVitorias (95, 20)
+let nivelFinal = nivelHeroi ()
+console.log (`O Herói tem de saldo de ${saldo} está no nível ${nivelFinal}`)
+
+function saldoVitorias (vitorias, derrotas)
+{
+    return vitorias - derrotas
+}
+
+function nivelHeroi ()
+{
+    let nivel
+    switch (true)
+    {
+        case saldo <= 10:
+            nivel = "Ferro"
+            break
+
+        case saldo >= 11 && saldo <= 20:
+            nivel = "Bronze"
+            break
+
+        case saldo >= 21 && saldo <= 50:
+            nivel = "Prata"
+            break
+
+        case saldo >= 51 && saldo <= 80:
+            nivel = "Ouro"
+            break
+
+        case saldo >= 81 && saldo <= 90:
+            nivel = "Diamante"
+            break
+
+        case saldo >= 91 && saldo <= 100:
+            nivel = "Lendário"
+            break
+
+        case saldo > 100:
+            nivel = "Imortal"
+            break
+
+        default:
+            console.log ("Nivel do herói desconhecido!")
+            
+    }
+
+    return nivel
+}
--- funções/retornoEx.js
+++ funções/retornoEx.js
@@ -5,7 +5,7 @@ console.log ("bem vindo, " + userName)
 userName = getFirstName ("Igor-Dos-Santos-Amaral", "-")
 console.log ("bem vindo, " + userName)
 
-function getFirstName(name, splitChar)
+function getFirstName(name, splitChar = " ") //default
 {
     let firstName = name.split (splitChar) [0]
     //let firstName = name.split (" ") [0] //quebra cada lugar que tem espaço (" ") e coloca no vetor
